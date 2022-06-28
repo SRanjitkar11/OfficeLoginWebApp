80ce7a4a3d1436c17dbed8cf5f1c8a9e8fe19bb9
Suneer Ranjitkar <suneer.ranjitkar@vesuvio.io>
2022-06-28 05:56:43 +0000

    US26234, Users Name is Filtered and added separately in new table

---
+++ OfficeLoginWebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using OfficeLoginWebApp.Models;
+using OfficeLoginWebApp.Services;
+using System.Diagnostics;
+
+namespace OfficeLoginWebApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public const string SessionKeySaved = "CheckData";
+
+        private readonly ILogger<HomeController> _logger;
+        IDataService _ids;
+        public HomeController(ILogger<HomeController> logger, IDataService ids)
+        {
+            _logger = logger;
+            _ids = ids;
+        }
+
+        public IActionResult Index()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeySaved)))
+            {
+                _ids.LoginEntry(User.Identity.Name , Request.HttpContext.Connection.RemoteIpAddress.ToString());
+                HttpContext.Session.SetString(SessionKeySaved, "Saved");
+            }
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OfficeLoginWebApp/Migrations/20220628044047_FinalOne.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OfficeLoginWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "officeLoggers",
+                columns: table => new
+                {
+                    LoggerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoggerEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoginTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IPAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IPAddressLocal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LogoutTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_officeLoggers", x => x.LoggerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userSigninLogs",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoggerEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoginTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IPAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IPAddressLocal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LogoutTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userSigninLogs", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "officeLoggers");
+
+            migrationBuilder.DropTable(
+                name: "userSigninLogs");
+        }
+    }
+}
